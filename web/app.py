5de07fee4d9a5dbc3a17a3ff4bc228de919f1450
rin2401 <rin2401@gmail.com>
2021-01-13 01:27:12 +0700

    Fix import

--- web/app.py
+++ web/app.py
@@ -1,3 +1,6 @@
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+
 from flask import Flask, render_template, request, jsonify, abort, Markup, session
 from flask_socketio import SocketIO
 import json
